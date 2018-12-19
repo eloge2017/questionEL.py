5703dee3765abb0bd9c9f7d2e1dd3253315425ca
csPython <cspython@AD25TF04NGF1J-95.local>
2018-12-19 12:42:22 -0500

    wrong input feedback and  comments

--- questionEL01.py
+++ questionEL01.py
@@ -1,7 +1,11 @@
+#added the feedback that shows when you get it incorrect
+#INT variable
 c1 = False
 score = int(0)
 a1 = int(0)
+#while loop 
 while a1 == False:
+#question was asked and availabe options are shown 
     try:
         print("""How many fingers do you have on one hand?
 (1) 7
@@ -9,13 +13,21 @@ while a1 == False:
 (3) 9
 (4) 4""")
         a1 = int(input())
+#acceptable answers are set
         if 0 < a1 < 5:
            c1 = True
+#correct input is given a plus one
            if a1 == 2:
                score+=1
+#when feedback for when the input is correct
                print("Good job you got the question right.")
+#when feedback for when the input is incorrect
+           else:
+               print("try again your, answer was incorrect.")
+#feedback for when the input is not an option 
         else:
-            print("wrong answer try, please enter a different answer.")
+            print("wrong input, enter (1), (2), (3), or (4).")
+#feedback for when the input is not a number
     except ValueError:
         print("Please pick one of the number that your answer correspond to.")
 
