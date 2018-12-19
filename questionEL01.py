59c8b2ec7a075492a2eca9c3629dad2aabf65e3f
csPython <cspython@AD25TF04NGF1J-95.local>
2018-12-19 13:03:39 -0500

    restructured the while loop, comments easy to read, added the feedback at the end

--- questionEL01.py
+++ questionEL01.py
@@ -3,31 +3,29 @@
 c1 = False
 score = int(0)
 a1 = int(0)
-#while loop 
-while a1 == False:
-#question was asked and availabe options are shown 
-    try:
-        print("""How many fingers do you have on one hand?
+
+print("""How many fingers do you have on one hand?
 (1) 7
 (2) 5
 (3) 9
 (4) 4""")
+#while loop 
+while c1 == False:
+#question was asked and availabe options are shown 
+    try:
         a1 = int(input())
-#acceptable answers are set
-        if 0 < a1 < 5:
-           c1 = True
-#correct input is given a plus one
-           if a1 == 2:
-               score+=1
-#when feedback for when the input is correct
-               print("Good job you got the question right.")
-#when feedback for when the input is incorrect
-           else:
-               print("try again your, answer was incorrect.")
-#feedback for when the input is not an option 
+        if 0 < a1 < 5:    # test for acceptable answers
+            if a1 == 2:    # test for correct answer 
+               score+=1   # given a plus one
+               print("Good job.") # when feedback for when the input is correct
+            else:
+               print("Good job.")#when feedback for when the input is incorrect
+            c1 = True 
         else:
             print("wrong input, enter (1), (2), (3), or (4).")
-#feedback for when the input is not a number
+            #feedback for when the input is not an option
     except ValueError:
         print("Please pick one of the number that your answer correspond to.")
+        #feedback for when the input is not a whole number
+print("you got ", score * 100, "%.")
 
